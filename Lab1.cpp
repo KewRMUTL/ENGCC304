2eeb53a281d2e49c70cfe792c13a7434f534e905
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2024-10-05 18:04:07 +0700

    Lab1.cpp

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-        printf("Hello world\n");
-        printf("My name is Kew");
+        printf( "Hello world\n" ) ;
+        printf( "My name is Kew" ) ;
         return 0 ;
-} //end function
+}//end function
