7da96748dffa26b713329032e6227d2402c159ae
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2024-10-06 22:08:30 +0700

    67543206064-7_lab3.c

--- 67543206064-7_lab3.c
+++ 67543206064-7_lab3.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 
 int main() {
+
+
     int N ;
-    printf( "Enter N: " ) ;
+    printf( "Enter N : " ) ;
     scanf( "%d", &N ) ;
 
     int array[ N ] ;
     
     for ( int i = 0 ; i < N ; i++ ) {
-        printf( "Enter value[%d]: ", i ) ;
+        printf( "Enter value[%d] : ", i ) ;
         scanf( "%d", &array[ i ] ) ;
     }//end for
 
-    printf( "Index:\t" ) ;
+    printf( "Index:   " ) ;
     for ( int i = 0 ; i < N ; i++ ) {
         printf( "%2d ", i ) ;
     }//end for
-
-    printf( "\nArray:\t" ) ;
+    printf( "\nArray:   " ) ;
+    
     for ( int i = 0 ; i < N ; i++ ) {
         int isPrime = 1 ;
-        if ( array[i] < 2) isPrime = 0;
-        for ( int j = 2 ; j * j <= array[ i ]; j++ ) {
-            if ( array[ i ] % j == 0 ) {
+        if ( array[i] < 2) isPrime = 0 ;
+        for ( int j = 2; j * j <= array[ i ] ; j++ ) {
+            if ( array[i] % j == 0 ) {
                 isPrime = 0 ;
-                break;
+                break ;
             }//end if
+
         }//end for
         if ( isPrime ) {
             printf( "%2d ", array[ i ] ) ;
         } else {
             printf( " # " ) ;
         }//end if
+
+
     }//end for
     printf( "\n" ) ;
 
