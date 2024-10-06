269d891787238962aa9043225d9836ded7103c59
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2024-10-06 15:39:36 +0700

    67543206064-7_lab7.c

--- 67543206064-7_lab7.c
+++ 67543206064-7_lab7.c
@@ -3,16 +3,13 @@
 #include <time.h>
 
 int main() {
-    int play = 1, guess, winning_number ;
+    int play = 1, guess, winning_number = 42 ; 
     int score, lower_bound, upper_bound ;
-
-    srand( time( NULL ) ) ;
 
     while ( play == 1 ) {
         score = 100 ;
         lower_bound = 1 ;
         upper_bound = 100 ;
-        winning_number = 42 ; 
 
         printf( "Do you want to play game (1=play, -1=exit) : (Score=%d)\n", score ) ;
         scanf( "%d", &play ) ;
@@ -27,7 +24,7 @@ int main() {
 
             if ( guess < lower_bound || guess > upper_bound ) {
                 printf( "Your guess is out of the current bounds (%d-%d)! Try again.\n", lower_bound, upper_bound ) ;
-                continue ;  
+                continue ;
             }//end if
 
             score -= 10 ;
@@ -35,13 +32,13 @@ int main() {
             if ( guess == winning_number ) {
                 printf( "That is correct! The winning number is %d.\n", winning_number ) ;
                 printf( "Score this game: %d\n", score ) ;
-                break ;  
+                break ;
             } else if ( guess > winning_number ) {
-                printf( "Sorry, the winning number is LOWER than %d. (Score=%d)\n", guess, score ) ; 
-                upper_bound = guess - 1 ;  
+                printf( "Sorry, the winning number is LOWER than %d. (Score=%d)\n", guess, score ) ;
+                upper_bound = guess - 1 ;
             } else {
-                printf( "Sorry, the winning number is HIGHER than %d. (Score=%d)\n", guess, score ) ;
-                lower_bound = guess + 1 ; 
+                printf("Sorry, the winning number is HIGHER than %d. (Score=%d)\n", guess, score ) ;
+                lower_bound = guess + 1 ;
             }//end if
         }//end while
     }//end while
