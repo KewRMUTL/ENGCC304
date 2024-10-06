ed888dd8fff5b2ea55f7cda99ed208396e7e0e2f
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2024-10-06 14:34:36 +0700

    67543206064-7_lab9.c

--- 67543206064-7_lab9.c
+++ 67543206064-7_lab9.c
@@ -2,35 +2,31 @@
 
 int main() {
     int N, i ;
-  
+
     printf( "Enter N : " ) ;
     scanf( "%d", &N ) ;
-    
-    int array[ N ] ;
+
+    int arr[ N ] ;
     
     for ( i = 0 ; i < N ; i++ ) {
-        printf( "Enter value[ %d ] : ", i ) ;
-        scanf( "%d", &array[ i ]);
+        printf( "Enter value[%d] : ", i ) ;
+        scanf( "%d", &arr[ i ] ) ;
     }//end for
-    
+
     printf( "Index: " ) ;
-    for ( i = 0 ; i < N ; i++ ) {
+    for ( i = 0; i < N; i++ ) {
         printf( "%d ", i ) ;
     }//end for
-
-    printf( "\n" ) ;
     
+    printf( "\n" ) ;
     printf( "Array: " ) ;
     for ( i = 0 ; i < N ; i++ ) {
+        printf( "%d", arr[ i ] ) ;
         if ( i < N - 1 ) {
-            printf( "%d # ", array[ i ] ) ;
-        } else {
-            printf( "%d", array[ i ] ) ;
+            printf( " # " ) ;
         }//end if
     }//end for
-
     printf( "\n" ) ;
 
     return 0 ;
-
 }//end function
