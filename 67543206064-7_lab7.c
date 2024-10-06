96564ed33803d9997b5261299bca5ca85539e730
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2024-10-06 14:48:06 +0700

    67543206064-7_lab7.c

--- 67543206064-7_lab7.c
+++ 67543206064-7_lab7.c
@@ -12,7 +12,7 @@ int main() {
         score = 100 ;
         lower_bound = 1 ;
         upper_bound = 100 ;
-        winning_number = 42 ;
+        winning_number = 42 ; 
 
         printf( "Do you want to play game (1=play, -1=exit) : (Score=%d)\n", score ) ;
         scanf( "%d", &play ) ;
@@ -27,7 +27,7 @@ int main() {
 
             if ( guess < lower_bound || guess > upper_bound ) {
                 printf( "Your guess is out of the current bounds (%d-%d)! Try again.\n", lower_bound, upper_bound ) ;
-                continue ;
+                continue ;  
             }//end if
 
             score -= 10 ;
@@ -35,17 +35,16 @@ int main() {
             if ( guess == winning_number ) {
                 printf( "That is correct! The winning number is %d.\n", winning_number ) ;
                 printf( "Score this game: %d\n", score ) ;
-                break ;
-            } else if ( guess > winning_number) {
-                printf( "Sorry, the winning number is LOWER than %d. (Score=%d)\n", guess, score ) ;
-                upper_bound = guess - 1 ;
+                break ;  
+            } else if ( guess > winning_number ) {
+                printf( "Sorry, the winning number is LOWER than %d. (Score=%d)\n", guess, score ) ; 
+                upper_bound = guess - 1 ;  
             } else {
                 printf( "Sorry, the winning number is HIGHER than %d. (Score=%d)\n", guess, score ) ;
-                lower_bound = guess + 1 ;
+                lower_bound = guess + 1 ; 
             }//end if
         }//end while
     }//end while
-    
+
     return 0 ;
-    
 }//end function
