997acd0fd44f575b96282915650542a63b35b409
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2024-10-06 21:12:09 +0700

    67543206064-7_lab1.c

--- 67543206064-7_lab1.c
+++ 67543206064-7_lab1.c
@@ -16,3 +16,18 @@ int main() {
     
     return 0 ;
 }//end function
+
+#include <stduio.h>
+
+int main() {
+    char name[ 50 ] ;
+    int age ;
+    printf( "Enter your name: " ) 
+    scanf( "%s", name ) ;
+    printf( "Enter your age: " ) ;
+    scanf( "%d", &age ) ;
+    print( "- - - - - -\n" ) ;
+    printf( "Hello %s \n", name ) ;
+    printf( "Age = %d\n", age ) ;
+    
+}//end function
