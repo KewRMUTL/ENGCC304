229a5f898bda31bfe73a9c4875d2ea4f7cda1eca
KewRMUTL67 <phonphaijit_ch67@live.rmutl.ac.th>
2024-10-06 22:44:34 +0700

    67543206064-7_lab3.c

--- 67543206064-7_lab3.c
+++ 67543206064-7_lab3.c
@@ -1,44 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 
+int isPrime( int num ) { if( num < 2 ) return 0 ;
+    for ( int i = 2 ; i <= sqrt( num ) ; i++ ) {
+    if ( num % i == 0 )
+    return 0 ; 
+}//end function
+    return 1 ; }
 int main() {
-
-
-    int N ;
-    printf( "Enter N : " ) ;
-    scanf( "%d", &N ) ;
-
-    int array[ N ] ;
-    
-    for ( int i = 0 ; i < N ; i++ ) {
+    int n ;
+    printf( "Enter N : " ) ; 
+    scanf( "%d", &n ) ;
+    int arr[ n ] ; for( int i = 0 ; i < n ; i++ ){
         printf( "Enter value[%d] : ", i ) ;
-        scanf( "%d", &array[ i ] ) ;
-    }//end for
-
-    printf( "Index:   " ) ;
-    for ( int i = 0 ; i < N ; i++ ) {
+        scanf("%d", &arr[i] ) ;
+    }//end function
+    printf("Index:  ") ;
+    for (int i = 0 ; i < n ; i++ ){
         printf( "%2d ", i ) ;
     }//end for
-    printf( "\nArray:   " ) ;
-    
-    for ( int i = 0 ; i < N ; i++ ) {
-        int isPrime = 1 ;
-        if ( array[i] < 2) isPrime = 0 ;
-        for ( int j = 2; j * j <= array[ i ] ; j++ ) {
-            if ( array[i] % j == 0 ) {
-                isPrime = 0 ;
-                break ;
-            }//end if
-
-        }//end for
-        if ( isPrime ) {
-            printf( "%2d ", array[ i ] ) ;
-        } else {
-            printf( " # " ) ;
-        }//end if
-
-
-    }//end for
+    printf( "\n" );
+    printf( "Array:  " );
+    for( int i = 0 ; i < n ; i++ ) { if ( isPrime( arr[i] ) )
+    printf( "%2d ", arr[i] ) ; 
+    else
+    printf( "%2s ", "#" ) ; 
+    }//end if
     printf( "\n" ) ;
-
     return 0 ;
-}//end function
+}//end function main
